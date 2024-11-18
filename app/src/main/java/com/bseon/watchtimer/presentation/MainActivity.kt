227e2ec9f22fa1284a1eecb35c8ef11c77c113d7
dogdduddy <dogdduddy@gmail.com>
2024-11-18 23:56:58 +0900

    분 범위 0~59에서 1~60으로 수정

--- app/src/main/java/com/bseon/watchtimer/presentation/MainActivity.kt
+++ app/src/main/java/com/bseon/watchtimer/presentation/MainActivity.kt
@@ -28,13 +28,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Picker
 import androidx.wear.compose.material.PickerState
@@ -102,7 +100,11 @@ fun TimerTitle() {
 }
 
 @Composable
-fun TimerContent(timerState: MainViewModel.TimerState, pickerState: PickerState, timeLeft: Long) {
+fun TimerContent(
+    timerState: MainViewModel.TimerState,
+    pickerState: PickerState,
+    timeLeft: Long,
+) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -113,8 +115,9 @@ fun TimerContent(timerState: MainViewModel.TimerState, pickerState: PickerState,
             Picker(
                 state = pickerState,
                 contentDescription = "Number Picker",
-                separation = 1.dp,) {
-                Text(text = "$it", fontSize = 24.sp)
+                separation = 1.dp,
+            ) {
+                Text(text = "${pickerIndexToDisplay(it)}", fontSize = 24.sp)
             }
         } else {
             Text (
@@ -147,7 +150,9 @@ fun TimerButton(viewModel: MainViewModel, timerState: MainViewModel.TimerState, 
                         MainViewModel.TimerState.RUNNING -> viewModel.pauseTimer()
                         MainViewModel.TimerState.PAUSED -> viewModel.resumeTimer()
                         MainViewModel.TimerState.STOPPED -> {
-                            viewModel.setTimerDuration(pickerState.selectedOption.toMillis())
+                            viewModel.setTimerDuration(
+                                pickerIndexToDisplay(pickerState.selectedOption).toMillis()
+                            )
                             viewModel.startTimer()
                         }
                     }
@@ -186,4 +191,8 @@ fun Int.toMillis(): Long {
 
 fun Long.toMinutes(): Int {
     return (this / 1000 / 60).toInt()
+}
+
+fun pickerIndexToDisplay(index: Int): Int {
+    return index + 1
 }
