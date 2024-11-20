b98e0d983814d36580a264b49a2e095088e05a55
dogdduddy <dogdduddy@gmail.com>
2024-11-20 23:40:39 +0900

    웨이브 진동 효과 추가

--- app/src/main/java/com/bseon/watchtimer/presentation/VibrationHelper.kt
+++ app/src/main/java/com/bseon/watchtimer/presentation/VibrationHelper.kt
@@ -8,19 +8,43 @@ import android.os.VibratorManager
 
 class VibrationHelper(private val context: Context) {
 
-    fun vibrate() {
-        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    lateinit var vibrator: Vibrator
+
+    init {
+        setVibrator()
+    }
+
+    fun setVibrator() {
+        vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
             vibratorManager.defaultVibrator
         } else {
             context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         }
+    }
 
+    fun vibrate(duration: Long = 500) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val vibrationEffect = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
+            val vibrationEffect = VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE)
             vibrator.vibrate(vibrationEffect)
         } else {
-            vibrator.vibrate(500) // milliseconds
+            vibrator.vibrate(duration) // milliseconds
         }
+    }
+
+    fun waveVibrate(duration: Long = 500) {
+        val timings = longArrayOf(0, 100, 100, 100)
+        val amplitudes = intArrayOf(0, 255, 0, 255)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val vibrationEffect = VibrationEffect.createWaveform(timings, amplitudes, 0)
+            vibrator.vibrate(vibrationEffect)
+        } else {
+            vibrator.vibrate(timings, 0)
+        }
+    }
+
+    fun cancelVibrate() {
+        vibrator.cancel()
     }
 }
