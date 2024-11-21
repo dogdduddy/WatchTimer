244092b734b1c6efc96b48a22ca0031ed522e1d1
dogdduddy <dogdduddy@gmail.com>
2024-11-22 00:31:52 +0900

    버튼 ripple 원형으로 수정

--- app/src/main/java/com/bseon/watchtimer/presentation/TimerScreen.kt
+++ app/src/main/java/com/bseon/watchtimer/presentation/TimerScreen.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -93,7 +96,7 @@ fun TimerContent(
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
                 color = Color.White,
-                fontSize = 24.sp,
+                fontSize = 50.sp,
                 text = timeLeft.toMinutes().toString()
             )
         }
@@ -113,8 +116,8 @@ fun TimerButton(viewModel: MainViewModel, timerState: TimerState, pickerState: P
         Image(
             painter = painterResource(painterResource),
             modifier = Modifier
-                .height(30.dp)
-                .width(30.dp)
+                .size(30.dp)
+                .clip(CircleShape)
                 .clickable {
                     when(timerState) {
                         TimerState.RUNNING -> viewModel.pauseTimer()
@@ -132,7 +135,6 @@ fun TimerButton(viewModel: MainViewModel, timerState: TimerState, pickerState: P
                     }
                 },
             contentDescription = "Run Button",
-
             )
 
         if(timerState == TimerState.PAUSED) {
@@ -141,8 +143,8 @@ fun TimerButton(viewModel: MainViewModel, timerState: TimerState, pickerState: P
             Image(
                 painter = painterResource(R.drawable.ic_stop_btn),
                 modifier = Modifier
-                    .height(30.dp)
-                    .width(30.dp)
+                    .size(30.dp)
+                    .clip(CircleShape)
                     .clickable {
                         viewModel.stopTimer()
                     },
